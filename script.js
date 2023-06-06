a1556b6368923083603bef031fdc6b038203441b
Maekin <74455990+mekintown@users.noreply.github.com>
2023-06-06 08:42:08 +0700

    Add range input for pixel

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const grids = document.querySelector(".grids");
-const colorPicker = document.querySelector(".colorPicker")
+const colorPicker = document.querySelector(".colorPicker");
+const slideValue = document.querySelector(".sliderValue");
+const pixelPicker = document.querySelector(".pixelPicker");
+const root = document.querySelector(":root");
+
 let isMouseDown = false;
+
+createGrid(50);
+
 function createGrid(pixel) {
     grids.style.setProperty('grid-template-rows', `repeat(${pixel}, 1fr)`);
     grids.style.setProperty('grid-template-columns', `repeat(${pixel}, 1fr)`);
+    grids.textContent = '';
     for (let i = 0, stop = pixel**2; i < stop; i++) {
         const grid = document.createElement('div');
         grid.classList.add('grid');
@@ -21,5 +29,12 @@ function createGrid(pixel) {
     }
 }
 
+pixelPicker.addEventListener('input', () => {
+    slideValue.textContent = pixelPicker.value;
+    createGrid(pixelPicker.value);
+});
 
-createGrid(120);
+colorPicker.addEventListener('input', () => {
+    pixelPicker.style.setProperty('accent-color', `${colorPicker.value}`);
+    slideValue.style.setProperty('background-color', `${colorPicker.value}`);
+});
